e45353afbccf3b695f53740170866e599aca0141
Ryan Hashan <hashanjayy@gmail.com>
2026-07-21 01:56:09 +0530

    Updated Smooth-scroll

--- src/components/smooth-scroll.tsx
+++ src/components/smooth-scroll.tsx
@@ -17,7 +17,7 @@ export function setLenisScrollLocked(locked: boolean) {
 export function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.25,
+      duration: 0.9,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       touchMultiplier: 1.2,
